5a3ca924dd280179eb8768a963f76a9c2d9063a4
David Byman <dbvt@databass.com>
2014-03-23 05:24:53 -0400

    Create twitterCodes.js
    Twitter Codes.
    Since this is a parent of the public folder, no one in the public can see these.

---
+++ twitterCodes.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ * twitterCodes.js
+ * 
+ * Twitter API Codes.
+ * In a separate module to make fetching updates from GitHub easier
+ * 
+ */
+
+exports.getTwitterCodes = function() {
+
+    return {
+        consumer_key : "xxx",
+        consumer_secret : "xxx",
+        access_token_key : "xxx",
+        access_token_secret : "xxx"
+    };
+
+};
