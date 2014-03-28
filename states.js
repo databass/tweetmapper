af909ca80739c1537f93ed960e470c70f103abb9
David Byman <dbvt@databass.com>
2014-03-27 22:15:21 -0400

    Create states.js
    Module for States information, including search expression, and abbreviation.

---
+++ states.js
@@ -0,0 +1,186 @@
+/**
+ *
+ * states.js
+ *
+ * Information about each state (abbreviation, fullname, search RegExp Could
+ * also contain population weightings, and other info
+ *
+ */
+exports.getStates = function () {
+
+    return [{
+            "reg": "Idaho|ID",
+            "rs": "ID"
+        }, {
+            "reg": "Delaware|DE",
+            "rs": "DE"
+        }, {
+            "reg": "West Virginia|WV",
+            "rs": "WV"
+        }, {
+            "reg": "Virginia|VA",
+            "rs": "VA"
+        }, {
+            "reg": "Massachusetts|MA",
+            "rs": "MA"
+        }, {
+            "reg": "Montana|MT",
+            "rs": "MT"
+        }, {
+            "reg": "Michigan|MI",
+            "rs": "MI"
+        }, {
+            "reg": "Florida|FL",
+            "rs": "FL"
+        }, {
+            "reg": "Maine|ME",
+            "rs": "ME"
+        }, {
+
+            "reg": "Utah|UT",
+            "rs": "UT"
+        }, {
+
+            "reg": "Georgia|GA",
+            "rs": "GA"
+        },
+
+        {
+            "reg": "Kentucky|KY",
+            "rs": "KY"
+        }, {
+
+            "reg": "Arizona|AZ",
+            "rs": "AZ"
+        }, {
+
+            "reg": "Alabama|AL",
+            "rs": "AL"
+        }, {
+
+            "reg": "Mississippi|MS",
+            "rs": "MS"
+        }, {
+
+            "reg": "Tennessee|TN",
+            "rs": "TN"
+        }, {
+
+            "reg": "Wyoming|WY",
+            "rs": "WY"
+        }, {
+
+            "reg": "Oklahoma|OK",
+            "rs": "OK"
+        }, {
+
+            "reg": "Washington DC|DC",
+            "rs": "DC"
+        }, {
+            "reg": "Nevada|NV",
+            "rs": "NV"
+        }, {
+            "reg": "California|CA",
+            "rs": "CA"
+        }, {
+            "reg": "Oregon|OR",
+            "rs": "OR"
+        }, {
+            "reg": "Illinois|IL",
+            "rs": "IL"
+        }, {
+            "reg": "Vermont|VT",
+            "rs": "VT"
+        }, {
+            "reg": "South Dakota|SD",
+            "rs": "SD"
+        }, {
+            "reg": "North Dakota|ND",
+            "rs": "ND"
+        }, {
+            "reg": "Minnesota|MN",
+            "rs": "MN"
+        }, {
+            "reg": "Iowa|IA",
+            "rs": "IA"
+        }, {
+            "reg": "Wisconsin|WI",
+            "rs": "WI"
+        }, {
+            "reg": "New Hampshire|NH",
+            "rs": "NH"
+        }, {
+            "reg": "New York",
+            "rs": "NY"
+        }, {
+            "reg": "New Jersey",
+            "rs": "NJ"
+        }, {
+            "reg": "Maryland",
+            "rs": "MD"
+        }, {
+            "reg": "Missouri",
+            "rs": "MO"
+        }, {
+            "reg": "Nebraska|NE",
+            "rs": "NE"
+        }, {
+            "reg": "Kansas|KS",
+            "rs": "KS"
+        }, {
+            "reg": "New Mexico|NM",
+            "rs": "NM"
+        }, {
+            "reg": "Colorado|CO",
+            "rs": "CO"
+        }, {
+            "reg": "Texas|TX",
+            "rs": "TX"
+        }, {
+            "reg": "Idaho|ID",
+            "rs": "ID"
+        }, {
+            "reg": "Florida|FL",
+            "rs": "FL"
+        }, {
+            "reg": "Indiana|IN",
+            "rs": "IN"
+        }, {
+            "reg": "Ohio|OH",
+            "rs": "OH"
+        }, {
+            "reg": "South Carolina|SC",
+            "rs": "SC"
+        }, {
+            "reg": "North Carolina|NC",
+            "rs": "NC"
+        }, {
+            "reg": "Hawaii|HI",
+            "rs": "HI"
+        }, {
+            "reg": "Louisiana|LA",
+            "rs": "LA"
+        }, {
+            "reg": "Arkansas|AR",
+            "rs": "AR"
+        }, {
+            "reg": "Rhode Island|RI",
+            "rs": "RI"
+        }, {
+            "reg": "Pennsylvania|PA",
+            "rs": "PA"
+        }, {
+            "reg": "Wyoming|WY",
+            "rs": "WY"
+        }, {
+            "reg": "Connecticut|CT",
+            "rs": "CT"
+        }, {
+            "reg": "Alaska|AK",
+            "rs": "AK"
+        }, {
+            "reg": "Washington|WA",
+            "rs": "WA"
+        }];
+
+};
